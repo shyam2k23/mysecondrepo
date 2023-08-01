66f6e21644604b9398dc561ef8728b10c559a6bf
ShyamS <shyamssmail@gmail.com>
2023-08-01 17:08:12 +0530

    java project added

---
+++ src/main/java/com/example/seconddemo/SecondDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.seconddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondDemoApplication.class, args);
+	}
+
+}
